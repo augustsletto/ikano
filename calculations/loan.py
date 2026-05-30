215c2e46617e71ea704d2757e257db5c118482ef
augustsletto <augustsletto@gmail.com>
2026-05-30 17:44:42 +0200

    error handling semi done

--- calculations/loan.py
+++ calculations/loan.py
@@ -13,27 +13,32 @@ Round money clearly.
 from decimal import Decimal, ROUND_HALF_UP
 
 def loan(principal: float, annual_rate: float, months: int) -> float:
-    try:
-        float(principal)
-        float(annual_rate)
-        int(months)
-    except ValueError:
-        return "invalid input"
     
-    if annual_rate == 0:
-        return "Zero interest is not allowed."
-    
-    P = Decimal(str(principal))
-    r = Decimal(str(annual_rate)) / Decimal("12")
-    n = months
-    
-    # loan formula
-    M = P * (r*(1+r)**n / ((1+r)**n - 1))
-    
+    if isinstance(principal, (int, float)) and isinstance(annual_rate, (int, float)) and isinstance(months, int):
+        
+        
+        
+        # handle 0 as input value
+        inputs = {"principal": principal, "annual_rate":annual_rate, "months":months}
+        for key, value in inputs.items():
+            if value <= 0:
+                return f"{value} Not valid. '{key}' Must be higher than "
+        
+        P = Decimal(str(principal))
+        r = Decimal(str(annual_rate)) / Decimal("12")
+        n = months
+        
+        # loan formula
+        M = P * (r*(1+r)**n / ((1+r)**n - 1))
+        
 
-    return M.quantize(Decimal("0.01"), ROUND_HALF_UP) # round up, two decimal places
+        return M.quantize(Decimal("0.01"), ROUND_HALF_UP) # round up, two decimal places
+    return {
+        "type(principal) should be int or float": type(principal),
+        "type(annual_rate) should be int or float": type(annual_rate),
+        "type(months) should be int": type(months),
+    }
     
     
-    
-l = loan(100_000, 0.05, 48)
-print("monthly payment: ", l) # 100k, 5%, 48 months
+l = loan(100000.0, 0.05, 48.1)
+print(l) # 100k, 5%, 48 months
